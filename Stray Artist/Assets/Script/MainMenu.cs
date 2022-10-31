bccb3042053ccf18b5a5e9d99da6c9e0c047ede5
BreadButterCat <40633228+tulza@users.noreply.github.com>
2022-10-31 22:50:44 +1300

    Minor UI changes to main menu and escape menu

--- Stray Artist/Assets/Script/MainMenu.cs
+++ Stray Artist/Assets/Script/MainMenu.cs
@@ -5,26 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] GameObject AchievementUI;
     [SerializeField] GameObject ButtonUI;
     void Start() {
-        AchievementUI.SetActive(false);
         ButtonUI.SetActive(true);
     }
-
-    //These methods runs inside unity and does not require Start() or Update()
-    public void Achievement()
-    {
-        Debug.Log("Load Achievement Menu");
-       AchievementUI.SetActive(true);
-       ButtonUI.SetActive(false);
-    }
-    public void UnLoadAchievement()
-    {
-        Debug.Log("UnLoading Achievement Menu");
-       AchievementUI.SetActive(false);
-       ButtonUI.SetActive(true);
-    }      
 
     public void NewGame()
     {
