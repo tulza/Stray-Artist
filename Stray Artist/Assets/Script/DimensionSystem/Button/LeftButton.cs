a0b76f7f02a5781f3bdf7a4f7bf43d9af3016911
BreadButterCat <40633228+tulza@users.noreply.github.com>
2022-11-05 13:06:19 +1300

    Arrow indicator UI

--- Stray Artist/Assets/Script/DimensionSystem/Button/LeftButton.cs
+++ Stray Artist/Assets/Script/DimensionSystem/Button/LeftButton.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class LeftButton : MonoBehaviour
 {
+    //UI
+    public GameObject ArrowUIPrefab;
+    public CanvasGroup InstanceArrowUIPrefab;
     bool isOnButton = false;
+
+    private void Start() {
+        //Instance UI and set alpha to 0
+        InstanceArrowUIPrefab= GameObject.Instantiate(ArrowUIPrefab).transform.GetChild(0).GetChild(0).GetComponent<CanvasGroup>();
+        InstanceArrowUIPrefab.alpha = 0;
+    }
+
     void Update()
     {
         //change stage to the left of the machine
         if(Input.GetKeyDown(KeyCode.E) && isOnButton == true)
         {
             FindObjectOfType<PortalManager>().ChangeStageLeft();
+        }
+    ArrowIndicator();
+    }
+
+    void ArrowIndicator(){
+        //Fade in UI if on arrow
+        if(isOnButton){
+            InstanceArrowUIPrefab.alpha += 2 * Time.deltaTime;
+        }
+        //fade out UI
+        else if(isOnButton == false){
+            InstanceArrowUIPrefab.alpha -= 2 * Time.deltaTime;
         }
     }
 
--- Stray Artist/Assets/Script/DimensionSystem/Button/RightButton.cs
+++ Stray Artist/Assets/Script/DimensionSystem/Button/RightButton.cs
@@ -2,15 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RightButton : MonoBehaviour
 {
+    //UI
+    public GameObject ArrowUIPrefab;
+    public CanvasGroup InstanceArrowUIPrefab;
     bool isOnButton = false;
+
+    private void Start() {
+        //Instance UI and set alpha to 0
+        InstanceArrowUIPrefab= GameObject.Instantiate(ArrowUIPrefab).transform.GetChild(0).GetChild(0).GetComponent<CanvasGroup>();
+        InstanceArrowUIPrefab.alpha = 0;
+    }
     void Update()
     {
         //change stage to the Right of the machine
         if(Input.GetKeyDown(KeyCode.E) && isOnButton == true)
         {
             FindObjectOfType<PortalManager>().ChangeStageRight();
+        }
+        ArrowIndicator();
+    }
+
+    void ArrowIndicator(){
+        //Fade in UI if on arrow
+        if(isOnButton){
+            InstanceArrowUIPrefab.alpha += 2 * Time.deltaTime;
+        }
+        //fade out UI
+        else if(isOnButton == false){
+            InstanceArrowUIPrefab.alpha -= 2 * Time.deltaTime;
         }
     }
 
